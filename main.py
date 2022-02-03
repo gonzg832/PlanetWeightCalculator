4fdbbdb322ab5a47d780fe0bb737b1ed5a63f8b7
Geovanni Gonzalez <91762238+gonzg832@users.noreply.github.com>
2022-02-03 23:22:17 +0000

    Added locations for outputs and moved the 'calculate' button next to the input box

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import PySimpleGUI as sg
 
+
 sg.theme('DarkBlack1')
 
 layout = [
-    [sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show'), sg.Button('Exit')]
+    [sg.Text('Your Weight on Other Planets')],
+          [sg.Input(size=(6,1), key='-WEIGHT-'), sg.Button('Calculate')],
+          [
+              [sg.Text("Mercury:"),sg.Text(size=(15,1), key='-MERCURY_OUTPUT-')],
+              [sg.Text("Mars:     "),sg.Text(size=(15,1), key='-MARS_OUTPUT-')],
+              [sg.Text("Jupiter:   "),sg.Text(size=(15,1), key='-JUPITER_OUTPUT-')],
+              [sg.Text("Pluto:     "),sg.Text(size=(15,1), key='-PLUTO_OUTPUT-')],
+          ],
+          [sg.Button('Exit')]
           ]
 
-window = sg.Window('Weight on Other Planets', layout)
+window = sg.Window('Weight on Planets Calculator', layout)
 
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == 'Show':
+    if event == 'Calculate':
         # Update the "output" text element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
+        window['-MERCURY_OUTPUT-'].update(values['-WEIGHT-'])
+        window['-MARS_OUTPUT-'].update(values['-WEIGHT-'])
+        window['-JUPITER_OUTPUT-'].update(values['-WEIGHT-'])
+        window['-PLUTO_OUTPUT-'].update(values['-WEIGHT-'])
 
 window.close()
