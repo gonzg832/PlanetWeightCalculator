197d8e281eb74f257290a16a172b57ab768a48d3
Geovanni Gonzalez <91762238+gonzg832@users.noreply.github.com>
2022-02-07 23:24:19 +0000

    Added controller, formula functions, outputs different weights and added the rest of the planets

---
+++ controller.py
@@ -0,0 +1,31 @@
+def mercury_weight(weight):
+    weight = float(weight)
+    return round(weight * 0.38, 2)
+
+def venus_weight(weight):
+    weight = float(weight)
+    return round(weight * 0.91, 2)
+
+def mars_weight(weight):
+    weight = float(weight)
+    return round(weight * 0.38, 2)
+
+def jupiter_weight(weight):
+    weight = float(weight)
+    return round(weight * 2.34, 2)
+
+def saturn_weight(weight):
+    weight = float(weight)
+    return round(weight * 1.06, 2)
+
+def uranus_weight(weight):
+    weight = float(weight)
+    return round(weight * 0.92, 2)
+
+def neptune_weight(weight):
+    weight = float(weight)
+    return round(weight * 1.19, 2)
+
+def pluto_weight(weight):
+    weight = float(weight)
+    return round(weight * 0.06, 2)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-
+import controller 
 
 sg.theme('DarkBlack1')
 
@@ -8,8 +8,12 @@ layout = [
           [sg.Input(size=(6,1), key='-WEIGHT-'), sg.Button('Calculate')],
           [
               [sg.Text("Mercury:"),sg.Text(size=(15,1), key='-MERCURY_OUTPUT-')],
+              [sg.Text("Venus:   "),sg.Text(size=(15,1), key='-VENUS_OUTPUT-')],
               [sg.Text("Mars:     "),sg.Text(size=(15,1), key='-MARS_OUTPUT-')],
               [sg.Text("Jupiter:   "),sg.Text(size=(15,1), key='-JUPITER_OUTPUT-')],
+              [sg.Text("Saturn:  "),sg.Text(size=(15,1), key='-SATURN_OUTPUT-')],
+              [sg.Text("Uranus:  "),sg.Text(size=(15,1), key='-URANUS_OUTPUT-')],
+              [sg.Text("Neptune:"),sg.Text(size=(15,1), key='-NEPTUNE_OUTPUT-')],
               [sg.Text("Pluto:     "),sg.Text(size=(15,1), key='-PLUTO_OUTPUT-')],
           ],
           [sg.Button('Exit')]
@@ -23,10 +27,30 @@ while True:  # Event Loop
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == 'Calculate':
-        # Update the "output" text element to be the value of "input" element
-        window['-MERCURY_OUTPUT-'].update(values['-WEIGHT-'])
-        window['-MARS_OUTPUT-'].update(values['-WEIGHT-'])
-        window['-JUPITER_OUTPUT-'].update(values['-WEIGHT-'])
-        window['-PLUTO_OUTPUT-'].update(values['-WEIGHT-'])
+        weight = values['-WEIGHT-']
+        
+        mercury_weight = controller.mercury_weight(weight)
+        window['-MERCURY_OUTPUT-'].update(mercury_weight)
+
+        venus_weight = controller.venus_weight(weight)
+        window['-VENUS_OUTPUT-'].update(venus_weight)
+       
+        mars_weight = controller.mars_weight(weight)
+        window['-MARS_OUTPUT-'].update(mars_weight)
+        
+        jupiter_weight = controller.jupiter_weight(weight)
+        window['-JUPITER_OUTPUT-'].update(jupiter_weight)
+        
+        saturn_weight = controller.saturn_weight(weight)
+        window['-SATURN_OUTPUT-'].update(saturn_weight)
+
+        uranus_weight = controller.uranus_weight(weight)
+        window['-URANUS_OUTPUT-'].update(uranus_weight)
+
+        neptune_weight = controller.neptune_weight(weight)
+        window['-NEPTUNE_OUTPUT-'].update(neptune_weight)
+
+        pluto_weight = controller.pluto_weight(weight)
+        window['-PLUTO_OUTPUT-'].update(pluto_weight)
 
 window.close()
